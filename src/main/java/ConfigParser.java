afbd75389c3d20cc7f0af8ada67e4460c8d8587e
Kene-Michael <mac@kaycees-MacBook-Pro.local>
2020-08-05 09:28:04 +0100

    Commented code and tackled arguments

--- src/main/java/ConfigParser.java
+++ src/main/java/ConfigParser.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.*;
@@ -10,40 +11,63 @@ public class ConfigParser {
 
     public ConfigParser(String fileName) {
         this.fileName = "src/main/java/" + fileName;
-        Path path = FileSystems.getDefault().getPath(this.fileName);
+        File file = new File(this.fileName);
+        String filePath = file.exists() ? this.fileName : fileName;
+        Path path = FileSystems.getDefault().getPath(filePath);
+        configParser(path);
+    }
+
+    /**
+     * This method takes in the path of a file, uses Scanner to read lines of this file,manipulate the words gotten
+     * in such a way that they can be stored successfully in a map using keys as specified.
+     * @param path
+     */
+    public void configParser(Path path) {
         try(Scanner input = new Scanner(path)) {
             while (input.hasNextLine()) {
-                String line = input.nextLine();
-                if(line.equals("")) {
+                String line = input.nextLine();//reads a whole lin and stores it in a variable line.
+                if(line.equals("")) { //checks if read line is empty and if true moves to next line.
                     continue;
                 }
-                if(line.contains("[")) {
+                if(line.contains("[")) { //checks if the read line contains [.
+                    /*if the line contains [, replace all special characters with a space, remove all leading and trailing
+                    spaces and add a . to the line then read the next line.*/
                     line = line.replaceAll("[^a-zA-Z0-9]", " ").trim() + ".";
-//                    System.out.println(line);
-                    String[] arr = input.nextLine().split("=");
-                    String key = line + arr[0];
-                    if(configMap.containsKey(key)) {
-                        continue;
+
+                    String data = input.nextLine();// reading next line and storing in a variable data.
+
+                    while (input.hasNextLine() && !data.trim().equals("")) {//a loop to keep looking for a next line until an empty line is found
+                        String[] arr = data.split("=");/* split data into an array at the equals sign and add line to the
+                                                            beginning of the first element of the array using them as key in the map*/
+
+                        String key = line + arr[0];
+//                        System.out.println(Arrays.toString(arr));
+                        data = input.nextLine();
+                        if (configMap.containsKey(key)) {//checking if map already contains the key and if yes, just continue
+                            continue;
+                        }
+                        configMap.put(key, arr[1]); //if map does not contain key, put a new value using the key.
                     }
-                    configMap.put(key,arr[1]);
-                } else {
-                String[] arr = line.split("=");
+                } else { //handles case when original line is not empty and does not have a [.
+                    String[] arr = line.split("=");
                     if(configMap.containsKey(arr[0])) {
                         continue;
                     }
                     configMap.put(arr[0], arr[1]);
                 }
             }
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (NoSuchElementException | IOException e) { //catching Exceptions.
             e.printStackTrace();
         }
 
         System.out.println(configMap);
-
     }
 
+    /**
+     * This method takes in a key and return the value attached to the key from the Map storage.
+     * @param key
+     * @return String
+     */
     public String get(String key) {
         return configMap.get(key);
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
-import java.util.Map;
-
 public class Main {
 
 	public static void main(String[] args) {
+		/**
+		 * Checking if there is an argument passed and if yes, use it to determine file to read from.
+		 */
 		ConfigParser newConfig = null;
 		if(args.length == 0) {
 			 newConfig = new ConfigParser("config.txt");
 		}else if("staging".equals(args[0])) {
-			newConfig = new ConfigParser("config.txt.staging");
+			newConfig = new ConfigParser("config-staging.txt");
 		} else if("development".equals(args[0])) {
-			newConfig = new ConfigParser("config.txt.dev");
+			newConfig = new ConfigParser("config-dev.txt");
 		} else {
 			System.out.println("Invalid input");
 		}
@@ -17,6 +18,7 @@ public class Main {
 		System.out.println(newConfig.get("dbname"));
 		System.out.println(newConfig.get("host"));
 		System.out.println(newConfig.get("application.name"));
+		System.out.println(newConfig.get("application.port"));
 	}
 
 }
